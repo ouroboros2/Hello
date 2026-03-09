dd1c0c88ddf2a8e6008cea36b2a194913fc03598
Ron Villanueva <ron19.villanueva@gmail.com>
2026-03-09 16:33:56 +0800

    Third Commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,7 @@
 import org.apache.commons.lang3.StringUtils;
 
 public class Main {
-
     public static void main(String[] args) {
-
-        String text = "    hello IntelliJ         ";
-        String trimmed = StringUtils.trim(text);
-        System.out.println(trimmed);
-
+        System.out.println("Hello world");
     }
 }
