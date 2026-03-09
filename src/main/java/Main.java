a56045b6824da05578b47e0408f0f3044ea4e62e
Ron Villanueva <ron19.villanueva@gmail.com>
2026-03-09 15:41:16 +0800

    Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String text = "    hello Git         ";
+        String trimmed = StringUtils.trim(text);
+        System.out.println(trimmed);
+
+    }
+}
