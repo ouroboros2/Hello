db05d7a0dabb07fc81694504f8f2640466e215ca
Ron Villanueva <ron19.villanueva@gmail.com>
2026-03-09 15:53:05 +0800

    Second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String text = "    hello Git         ";
+        String text = "    hello IntelliJ         ";
         String trimmed = StringUtils.trim(text);
         System.out.println(trimmed);
 
